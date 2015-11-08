298a661f7cb4900a3c9ed294f2aa463dd9bcd6d4
tinouye <tinouye14@gmail.com>
2015-11-08 09:56:16 -0800

    Added 5th, 6th question

--- game.js
+++ game.js
@@ -5,7 +5,9 @@ var j=0;
 var k=0;
 var l=0;
 var m=0;
-var counter=[0,0,0,0];
+var n=0;
+var o=0;
+var counter=0;
 var score;
 
 var name = prompt("Welcome to the guessing game! What is your name?");
@@ -15,7 +17,7 @@ while(j===0){
 	guess1 = prompt("Am I 19 years old?");
 	alert("Your answer was: "+guess1);
 	if(guess1==="no"||guess1==="No"||guess1==="NO"||guess1==="N"||guess1==="n"){
-		counter[0]=1;
+		counter++;
 		alert("Nice one, "+name+"! I am actually 18.");
 		j=1;
 	}
@@ -31,7 +33,7 @@ while(k===0){
 	guess2 = prompt("Am I from Tokyo, Japan?");
 	alert("Your answer was: "+guess2);
 	if(guess2==="yes"||guess2==="Yes"||guess2==="YES"||guess2==="Y"||guess2==="y"){
-		counter[1]=1;
+		counter++;
 		alert("Nice job, "+name+"! I am indeed from Tokyo, Japan!");
 		k=1;
 	}
@@ -47,7 +49,7 @@ while(l===0){
 	guess3 = prompt("Am I allergic to peanuts?");
 	alert("Your answer was: "+guess3);
 	if(guess3==="yes"||guess3==="Yes"||guess3==="YES"||guess3==="Y"||guess3==="y"){
-		counter[2]=1;
+		counter++;
 		alert("Good guess, "+name+"! I am, in fact, allergic to peanuts.");
 		l=1;
 	}
@@ -59,26 +61,68 @@ while(l===0){
 		alert("Invalid input!");
 	}
 }
+
 while(m===0){
-	guess4 = prompt("Do I play soccer?");
-	/*not sure what numeric-answer question was (did we cover that?) so
-	here's a normal y/n question instead. Sorry... */
-	alert("Your answer was: "+guess4);
-	if(guess4==="yes"||guess4==="Yes"||guess4==="YES"||guess4==="Y"||guess4==="y"){
-		alert("Nice try "+name+". I play tennis but not soccer.");
+	guess4 = prompt("How many languages do I speak?");
+	alert("Your answer was " + guess4);
+	if(guess4==2){
+		alert("Correct! I speak English and Japanese.");
+		counter++;
 		m=1;
 	}
-	else if(guess4==="no"||guess4==="No"||guess4==="NO"||guess4==="N"||guess4==="n"){
+	else if(guess4<2||guess4>0){
+		alert("Actually, I speak two languages: English and Japanese.");
+		m=1;
+	}
+	else if (guess4>2){
+		alert("Sorry, I only speak two languages: English and Japanese.");
+		m=1;
+	}
+	else{
+		alert("Invalid input!");
+	}
+}
+
+while(n===0){
+	guess5 = prompt("Do I play soccer?");
+	/*not sure what numeric-answer question was (did we cover that?) so
+	here's a normal y/n question instead. Sorry... */
+	alert("Your answer was: "+guess5);
+	if(guess5==="yes"||guess5==="Yes"||guess5==="YES"||guess5==="Y"||guess4==="y"){
+		alert("Nice try "+name+". I play tennis but not soccer.");
+		n=1;
+	}
+	else if(guess5==="no"||guess5==="No"||guess5==="NO"||guess5==="N"||guess5==="n"){
 		alert("Good job "+name+"! I don't play soccer.");
 		counter[3]=1;
-		m=1;
+		counter++;
+		n=1;
 		}
 	else{
 		alert("Invalid input!");
 	}
 }
-score=counter[0]+counter[1]+counter[2]+counter[3];
-alert("You got " + score + " out of 4 correct");
+
+while(o===0){
+	guess6 = prompt("How many cats do I own?");
+	alert("Your answer was " + guess6);
+	if(guess6===0){
+		alert("That's right! I have no cats.");
+		o=1;
+		counter++;
+	}
+	else if (guess4>0){
+		alert("I actually don't own any cats.");
+		o=1;
+	}
+	else{
+		alert("Invalid input!");
+	}
+}
+
+alert("You got " + counter + " out of 6 correct");
+
+
 /*
 if(counter[0]===0){
 	alert("I am 18, not 19.");
