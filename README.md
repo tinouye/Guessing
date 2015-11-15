3ca308b7156de93c1cce0dda07b5f36cae7ee17d
tinouye <tinouye14@gmail.com>
2015-11-15 10:00:03 -0800

    Added images and style

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Guessing
 Created by Tomi Inouye
-This is a guessing game in which the user is asked yes or no questions regarding the author. The user must type in "yes", "Yes", "YES", "y", or  "Y" to answer yes, or "no", "No", "NO", "n", or "N" to answer no to the yes or no questions. To answer the numeric questions, the user must type arabic numerals. The code resides in game.js, and the game is run by running index.html.
+This is a guessing game in which the user is asked yes or no questions regarding the author.
+The user must type in "yes", "Yes", "YES", "y", or  "Y" to answer yes,
+or "no", "No", "NO", "n", or "N" to answer no to the yes or no questions.
+To answer the numeric questions, the user must type arabic numerals.
+The code resides in game.js, and the game is run by running index.html.
+Images files are in the 'images' directory.
 
 I didn't want to believe that my friend was a construction sign thief, but when I got home all the signs were there.
 
--- game.js
+++ game.js
@@ -1,15 +1,38 @@
 var guess1;
 var guess2;
 var guess3;
+var guess4;
+var guess5;
+var question6;
+var question = [];
+var correct = [];
+var wrong = [];
 var j=0;
-var k=0;
-var l=0;
-var m=0;
-var n=0;
-var o=0;
 var counter=0;
 var score;
 var isCorrect;
+
+question[0]= "Am I 19 years old?";
+question[1]= "Am I from Tokyo, Japan?";
+question[2]= "Am I allergic to peanuts?";
+question[3]= "How many languages do I speak?";
+question[4]= "Do I play soccer?";
+question[5]= "How many cats do I own?";
+
+correct[0]= "Nice one, "+name+"! I am actually 18.";
+correct[1]= "Nice job, "+name+"! I am indeed from Tokyo, Japan!";
+correct[2]= "Good guess, "+name+"! I am, in fact, allergic to peanuts.";
+correct[3]= "Correct! I speak English and Japanese.";
+correct[4]= "Good job "+name+"! I don't play soccer.";
+correct[5]= "That's right! I have no cats."
+
+wrong[0]= "Sorry "+name+", but I am actaully 18.";
+wrong[1]= "Too bad, "+name+". I am indeed from Tokyo, Japan.";
+wrong[2]= "Sorry "+name+", but I am, in fact, allergic to peanuts.";
+wrong[3]= ["Actually, I speak two languages: English and Japanese.",
+"Sorry, I only speak two languages: English and Japanese."];
+wrong[4]= "Nice try "+name+". I play tennis but not soccer.";
+wrong[5]= "I actually don't own any cats.";
 
 function yesNo(a,b){
 	alert("Your answer was: "+a);
@@ -43,7 +66,7 @@ function yesNo(a,b){
 				j=1;
 			}
 
-			else if(a<b||a>=0){
+			else if(a<b&&a>=0){
 				isCorrect="tooSmall";
 				j=1;
 			}
@@ -60,99 +83,115 @@ function yesNo(a,b){
 
 var name = prompt("Welcome to the guessing game! What is your name?");
 alert("Welcome, " + name + "!");
-alert('Rules: answers must be "yes" or "no".');
 
+//QUESTION 1 - AGE
 while(j===0){
-	guess1 = prompt("Am I 19 years old?");
+	guess1 = prompt(question[0]);
 
 	yesNo(guess1,false);
-	console.log(isCorrect);
 }
 	if(isCorrect===true){
-		document.getElementById('q1').innerHTML="Nice one, "+name+"! I am actually 18.";
+		document.getElementById('i1').src='images/correct.png'
+		document.getElementById('q1').style.color = 'green';
+		document.getElementById('q1').innerHTML = correct[0];
 	}
 	else if(isCorrect===false){
-		document.getElementById('q1').innerHTML="Sorry "+name+", but I am actaully 18.";
+		document.getElementById('i1').src = 'images/incorrect.png'
+		document.getElementById('q1').style.color = 'red';
+		document.getElementById('q1').innerHTML = wrong[0];
 	}
 j=0;
 
+//QUESTION 2 - ORIGIN
 while(j===0){
-	guess2 = prompt("Am I from Tokyo, Japan?");
+	guess2 = prompt(question[1]);
 
 	yesNo(guess2,true);
 }
 	if(isCorrect===true){
-		document.getElementById('q2').innerHTML="Nice job, "+name+"! I am indeed from Tokyo, Japan!";
+		document.getElementById('i2').src = 'images/correct.png'
+		document.getElementById('q2').style.color = 'green';
+		document.getElementById('q2').innerHTML = correct[1];
 	}
 	else if(isCorrect===false){
-		document.getElementById('q2').innerHTML="Too bad, "+name+". I am indeed from Tokyo, Japan.";
+		document.getElementById('i2').src = 'images/incorrect.png'
+		document.getElementById('q2').style.color = 'red';
+		document.getElementById('q2').innerHTML = wrong[1];
 	}
 j=0;
 
+//QUESTION 3 - ALLERGY
 while(j===0){
-	guess3 = prompt("Am I allergic to peanuts?");
+	guess3 = prompt(question[2]);
 	yesNo(guess3,true);
 }
 	if(isCorrect===true){
-		document.getElementById('q3').innerHTML="Good guess, "+name+"! I am, in fact, allergic to peanuts.";
+		document.getElementById('i3').src = 'images/correct.png'
+		document.getElementById('q3').style.color = 'green';
+		document.getElementById('q3').innerHTML = correct[2];
 	}
 	else if(isCorrect===false){
-		document.getElementById('q3').innerHTML="Sorry "+name+", but I am, in fact, allergic to peanuts.";
+		document.getElementById('i3').src = 'images/incorrect.png'
+		document.getElementById('q3').style.color = 'red';
+		document.getElementById('q3').innerHTML = wrong[2];
 	}
 j=0;
 
+//QUESTION 4 - LANGUAGE
 while(j===0){
-	guess4 = Number(prompt("How many languages do I speak?"));
+	guess4 = Number(prompt(question[3]));
 	yesNo(guess4,2);
 }
 	if(isCorrect===true){
-		document.getElementById('q4').innerHTML="Correct! I speak English and Japanese.";
+		document.getElementById('i4').src = 'images/correct.png'
+		document.getElementById('q4').style.color = 'green';
+		document.getElementById('q4').innerHTML = correct[3];
 	}
 	else if(isCorrect==="tooSmall"){
-		document.getElementById('q4').innerHTML="Actually, I speak two languages: English and Japanese.";
+		document.getElementById('i4').src = 'images/incorrect.png'
+		document.getElementById('q4').style.color = 'red';
+		document.getElementById('q4').innerHTML = wrong[3][0];
 	}
 	else if(isCorrect==="tooBig"){
-		document.getElementById('q4').innerHTML="Sorry, I only speak two languages: English and Japanese.";
+		document.getElementById('i4').src = 'images/incorrect.png'
+		document.getElementById('q4').style.color = 'red';
+		document.getElementById('q4').innerHTML = wrong[3][1];
 	}
 j=0;
 
+//QUESTION 5 - SPORTS
 while(j===0){
-	guess5 = prompt("Do I play soccer?");
+	guess5 = prompt(question[4]);
 	yesNo(guess5,false);
 }
-	if(isCorrect===false){
-		document.getElementById('q5').innerHTML="Nice try "+name+". I play tennis but not soccer.";
+	
+	if(isCorrect===true){
+		document.getElementById('i5').src = 'images/correct.png'
+		document.getElementById('q5').style.color = 'green';
+		document.getElementById('q5').innerHTML = correct[4];
 	}
-	else if(isCorrect===true){
-		document.getElementById('q5').innerHTML="Good job "+name+"! I don't play soccer.";
+	if(isCorrect===false){
+		document.getElementById('i5').src = 'images/incorrect.png'
+		document.getElementById('q5').style.color = 'red';
+		document.getElementById('q5').innerHTML = wrong[4];
 	}
 j=0;
 
-
+//QUESTION 6 - CATS
 while(j===0){
-	guess6 = Number(prompt("How many cats do I own?"));
+	guess6 = Number(prompt(question[5]));
 	yesNo(guess6,0);
 }
 	if(isCorrect===true){
-		document.getElementById('q6').innerHTML="That's right! I have no cats.";
+		document.getElementById('i6').src = 'images/correct.png'
+		document.getElementById('q6').style.color = 'green';
+		document.getElementById('q6').innerHTML = correct[5];
 	}
 	else if(isCorrect!=true){
-		document.getElementById('q6').innerHTML="I actually don't own any cats.";
+		document.getElementById('i6').src = 'images/incorrect.png'
+		document.getElementById('q6').style.color = 'red';
+		document.getElementById('q6').innerHTML = wrong[5];
 	}
-j=0;
 
 
 //alert("You got " + counter + " out of 6 correct");
-
-
-/*
-if(counter[0]===0){
-	alert("I am 18, not 19.");
-}
-if(counter[1]===0){
-	alert("I am indeed from Tokyo, Japan.");
-}
-if(counter[2]===0){
-	alert("I am, in fact, allergic to peanuts.");
-}
-*/
